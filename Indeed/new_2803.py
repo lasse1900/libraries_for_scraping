e27c395df69b950556b9426aafa610e55de9a5ab
Lauri Kyttälä <lauri.kyttala@etteplan.com>
2023-03-29 09:48:30 +0300

    tried to modify Indeed script new_2803.py - not working (doesnt close \"banners\")

--- Indeed/new_2803.py
+++ Indeed/new_2803.py
@@ -15,17 +15,5 @@ search_field = browser.find_element(by=By.XPATH, value=input_field)
 search_field.send_keys("embedded")
 search_button = browser.find_element(By.XPATH,value=search_button)
 search_button.click()
-
-
-
-
-# username.send_keys("lauri.kyttala@gmail.com")
-# password=browser.find_element(By.XPATH,value="session_password")
-# # password=browser.find_element(By.CSS_SELECTOR,'/html/body/main/section[1]/div/div/form[1]/div[1]/div[2]/div/div/input')
-# username.send_keys("TAmmiholma8")
-
-# login_button=browser.find_element_by_class_name("sign-in-form__submit-button")
-# login_button=browser.find_element(By.XPATH,value="//*[@id='main-content']/section[1]/div/div/form[1]/div[2]/button")
-# login_button.click()
-
-# browser.get("https://www.linkedin.com/")
+browser.implicitly_wait(2)
+browser.get_screenshot_as_file('indeed.png') 

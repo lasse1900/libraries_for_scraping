3cbffe085830eb11311de32af0a3a42aefcacbda
Lauri Kyttälä <lauri.kyttala@etteplan.com>
2023-04-06 13:39:29 +0300

    corrected JSON layout to rabidAPI_basic_B.py + testing __main__ names

---
+++ Rapid_API_Indeed_New/Module_names/first_module.py
@@ -0,0 +1,11 @@
+
+print("This will always be run")
+
+def main():
+    print("First module's Main method")
+
+if __name__ == '__main__':
+#     print("Run directly")
+# else:
+#     print("Run from import")
+    main()
---
+++ Rapid_API_Indeed_New/Module_names/second_module.py
@@ -0,0 +1,5 @@
+import first_module
+
+first_module.main()
+
+print("Second module's name is: {}".format(__name__))

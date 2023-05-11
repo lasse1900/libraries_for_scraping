efad7dec3456bc6753efb171c18c7525703f6b48
Lauri Kyttälä <lauri.kyttala@etteplan.com>
2023-05-11 21:08:16 +0300

    added dir Basic_Loop_Request, Main_idiom, Send_Mail (Outlook) - point out -> request_loop_MAN_inputs

--- Basic_Loop_Request/plain_request.py
+++ Basic_Loop_Request/plain_request.py
@@ -1,11 +1,17 @@
 import requests
+import os
+from dotenv import load_dotenv
+import time
+
+load_dotenv()
+token = os.environ.get("X-RapidAPI-Key")
 
 url = "https://indeed-jobs-api-finland.p.rapidapi.com/indeed-fi/"
 
 querystring = {"offset":"0","keyword":"python developer","location":"Helsinki"}
 
 headers = {
-	"X-RapidAPI-Key": "2c36597583mshb3ec6f540dfd966p181965jsne63a63911509",
+	"X-RapidAPI-Key": token,
 	"X-RapidAPI-Host": "indeed-jobs-api-finland.p.rapidapi.com"
 }
 
